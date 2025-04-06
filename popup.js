186a1d978f5dee7172c9757fc6f2be6a5454a9b6
rekmixa <rekmixa@gmail.com>
2025-04-07 02:26:15 +0500

    draft: add oneColorGo option

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const ogsAiDisabledOption = document.getElementById('ogsAiDisabled')
   const ogsHideGameStateOption = document.getElementById('ogsHideGameState')
+  const oneColorGoOption = document.getElementById('oneColorGo')
   const storage = chrome.storage.sync || chrome.storage.local
 
   storage.get('ogsAiDisabled', function (data) {
@@ -11,11 +12,19 @@ document.addEventListener('DOMContentLoaded', function () {
     ogsHideGameStateOption.checked = data.ogsHideGameState || false
   })
 
+  storage.get('oneColorGo', function (data) {
+    oneColorGoOption.checked = data.oneColorGo || false
+  })
+
   ogsAiDisabledOption.addEventListener('change', function () {
     storage.set({ ogsAiDisabled: ogsAiDisabledOption.checked })
   })
 
   ogsHideGameStateOption.addEventListener('change', function () {
     storage.set({ ogsHideGameState: ogsHideGameStateOption.checked })
+  })
+
+  oneColorGoOption.addEventListener('change', function () {
+    storage.set({ oneColorGo: oneColorGoOption.checked })
   })
 })
